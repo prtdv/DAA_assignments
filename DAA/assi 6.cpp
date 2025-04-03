10ed03b440e9aea5198f1ebb344e4c4eab082f7a
pankajmores <136971374+pankajmores@users.noreply.github.com>
2025-04-03 21:36:18 +0530

    Create assi 6.cpp

---
+++ DAA/assi 6.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int knapsack(int W, vector<int> &wt, vector<int> &val, int n) {
+   vector<vector<int> > dp(n + 1, vector<int>(W + 1, 0));
+
+    for (int i = 1; i <= n; i++) {
+        for (int w = 0; w <= W; w++) {
+            if (wt[i - 1] <= w){
+                dp[i][w] = max(dp[i - 1][w], val[i - 1] + dp[i - 1][w - wt[i - 1]]);
+            }
+            else{
+                dp[i][w] = dp[i - 1][w];
+            }
+        }
+    }
+    return dp[n][W];
+}
+
+int main() {
+    int n, W;
+    cout << "Number of items: ";
+    cin >> n;
+    
+    vector<int> wt(n), val(n);
+    cout << "Enter weight and value of each item:\n";
+    for (int i = 0; i < n; i++)
+        cin >> wt[i] >> val[i];
+    
+    cout << "Enter knapsack capacity: ";
+    cin >> W;
+    
+    cout << "Max value: " << knapsack(W, wt, val, n) << endl;
+    return 0;
+}
